45722f4818317c041b649d1ad4f90f3143f3bf11
Asus <annusallokiya6337@gmail.com>
2022-08-07 00:39:00 +0530

    "Day 2 Completed"

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firstflutter_app/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main()
@@ -10,11 +11,7 @@ class MyApp extends StatelessWidget
   Widget build(BuildContext context) {
     // TODO: implement build for UI
     return MaterialApp(
-      home:  Material(
-       child: Center( child: Container(
-            child: Text('WelCome to Flutter UI Works'),
-        ))
-      ),
+      home: HomePage(),
     );
   }
 
