a0bb3d447a3ac0473c49a621a48fd44dcfafc458
Asus <annusallokiya6337@gmail.com>
2022-08-08 00:21:18 +0530

    "Day 2 Completed"

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,20 @@ class MyApp extends StatelessWidget
     // TODO: implement build for UI
     return MaterialApp(
       home: HomePage(),
+      themeMode: ThemeMode.system,
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark
+        // dialogBackgroundColor: Colors.black45,
+        //     primarySwatch: Colors.blueGrey
+      ),
+      routes: {
+        "/":(context) => HomePage(),
+      },
+
     );
   }
 
